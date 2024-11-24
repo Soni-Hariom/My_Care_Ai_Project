b656e5174c8a1249550a957d46daab16567eba4a
Soni-Hariom <sonihariom555@gmail.com>
2024-11-24 23:47:04 +0530

    Folder structure added.

---
+++ template.py
@@ -0,0 +1,44 @@
+# Let's import all the required libraries
+import os
+import pathlib
+from pathlib import Path
+import logging
+
+#Logging String
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name='cnnClassifire'
+
+# .gitkeep file is need to create workflow folder on github
+list_of_file =[
+    ".github/workflows/.gitkeep", 
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trails.ipynb",
+    "templates/index.html",
+
+]
+
+for filepath in list_of_file:
+    filepath =  Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok = True)
+        logging.info(f"Creating directory;{filedir} for the file:{filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
